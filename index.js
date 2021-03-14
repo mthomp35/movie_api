bec6b4c7bf2b6ee04b7dbe16b7ec7a85e19a6cba
mthomp35 <mariethompson35@gmail.com>
2021-03-14 15:40:27 -0700

    remove jwt token req from /movies

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 });
 
 // Return list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
